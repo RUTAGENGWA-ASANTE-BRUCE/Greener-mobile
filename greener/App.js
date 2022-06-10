8ab596a686d6ba164f7bfb96fd8e595a99a177ec
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-10 12:10:55 +0200

    new change

--- greener/App.js
+++ greener/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import {SafeAreaProvider} from "react-native-safe-area-context"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import {NavigationContainer} from "@react-navigation/native"
 import HomeScreen from "./screens/HomeScreen"
+import LoginScreen from "./screens/LoginScreen"
+import BrowseScreen from "./screens/BrowseScreen"
 export default function App() {
+  const Stack=createNativeStackNavigator()
   return (
-    <SafeAreaView>
-      <HomeScreen/>
-      {/* <StatusBar style="auto" /> */}
-    </SafeAreaView>
+    <NavigationContainer>
+    <SafeAreaProvider>
+      
+        <Stack.Navigator>
+          <Stack.Screen component={HomeScreen} name="HomeScreen" options={{headerShown:false}}/>
+          <Stack.Screen component={LoginScreen} name="LoginScreen" options={{headerShown:false}} />
+          <Stack.Screen component={BrowseScreen} name="BrowseScreen" options={{headerShown:false}} />
+        </Stack.Navigator>
+    </SafeAreaProvider>
+      </NavigationContainer>
   );
 }
 
